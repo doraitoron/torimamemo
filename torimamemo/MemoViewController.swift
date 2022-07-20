3d12a2f4d72991160fc75d5ce96e0b6f688230ca
憲和　辰馬 <cl_s3206541025@soshi.jp>
2022-07-20 15:33:28 +0900

    復旧（オブジェクトのみ）

--- torimamemo/MemoViewController.swift
+++ torimamemo/MemoViewController.swift
@@ -24,21 +24,21 @@ class MemoViewController: UIViewController {
     
     @IBOutlet weak var WayTextField:UITextView!
     
-    @IBOutlet var ManButton:UIButton!
-    @IBOutlet var womanButton:UIButton!
-    @IBOutlet var Y1Button:UIButton!
-    @IBOutlet var Y2Button:UIButton!
-    @IBOutlet var Y3Button:UIButton!
-    @IBOutlet var Y4Button:UIButton!
-    @IBOutlet var Y5Button:UIButton!
+//    @IBOutlet var ManButton:UIButton!
+//    @IBOutlet var womanButton:UIButton!
+//    @IBOutlet var Y1Button:UIButton!
+//    @IBOutlet var Y2Button:UIButton!
+//    @IBOutlet var Y3Button:UIButton!
+//    @IBOutlet var Y4Button:UIButton!
+//    @IBOutlet var Y5Button:UIButton!
     
-    var switch_man:Bool=false
-    var switch_woman:Bool=false
-    var switch_Y1:Bool=false
-    var switch_Y2:Bool=false
-    var switch_Y3:Bool=false
-    var switch_Y4:Bool=false
-    var switch_Y5:Bool=false
+//    var switch_man:Bool=false
+//    var switch_woman:Bool=false
+//    var switch_Y1:Bool=false
+//    var switch_Y2:Bool=false
+//    var switch_Y3:Bool=false
+//    var switch_Y4:Bool=false
+//    var switch_Y5:Bool=false
     
     
     override func viewDidLoad() {
@@ -108,99 +108,99 @@ class MemoViewController: UIViewController {
                 }
         }
     
-    @IBAction func touchButton_man(_ sender: Any) {
-       print("ボタンが押されました")
-       if ( switch_man ) {
-         print("false")
-         switch_man = false
-           ManButton.backgroundColor = UIColor(red:1,green: 193/255,blue: 1,alpha: 1)
-       }
-       else {
-         print("true")
-         switch_man = true
-           ManButton.backgroundColor = UIColor.darkText
-       }
-     }
-    
-    @IBAction func touchButton_woman(_ sender: Any) {
-       print("ボタンが押されました")
-       if ( switch_woman ) {
-         print("false")
-         switch_woman = false
-           womanButton.backgroundColor = UIColor(red:1,green: 193/255,blue: 1,alpha: 1)
-       }
-       else {
-         print("true")
-         switch_woman = true
-           womanButton.backgroundColor = UIColor.darkText
-       }
-     }
-    
-    @IBAction func touchButton_Y1(_ sender: Any) {
-       print("ボタンが押されました")
-       if ( switch_Y1 ) {
-         print("false")
-         switch_Y1 = false
-           Y1Button.backgroundColor = UIColor(red:1,green: 193/255,blue: 1,alpha: 1)
-       }
-       else {
-         print("true")
-         switch_Y1 = true
-           Y1Button.backgroundColor = UIColor.darkText
-       }
-     }
-    @IBAction func touchButton_Y2(_ sender: Any) {
-       print("ボタンが押されました")
-       if ( switch_Y2 ) {
-         print("false")
-         switch_Y2 = false
-           Y2Button.backgroundColor = UIColor(red:1,green: 193/255,blue: 1,alpha: 1)
-       }
-       else {
-         print("true")
-         switch_Y2 = true
-           Y2Button.backgroundColor = UIColor.darkText
-       }
-     }
-    @IBAction func touchButton_Y3(_ sender: Any) {
-       print("ボタンが押されました")
-       if ( switch_Y3 ) {
-         print("false")
-         switch_Y3 = false
-           Y3Button.backgroundColor = UIColor(red:1,green: 193/255,blue: 1,alpha: 1)
-       }
-       else {
-         print("true")
-         switch_Y3 = true
-           Y3Button.backgroundColor = UIColor.darkText
-       }
-     }
-    @IBAction func touchButton_Y4(_ sender: Any) {
-       print("ボタンが押されました")
-       if ( switch_Y4 ) {
-         print("false")
-         switch_Y4 = false
-           Y4Button.backgroundColor = UIColor(red:1,green: 193/255,blue: 1,alpha: 1)
-       }
-       else {
-         print("true")
-         switch_Y4 = true
-           Y4Button.backgroundColor = UIColor.darkText
-       }
-     }
-    @IBAction func touchButton_Y5(_ sender: Any) {
-       print("ボタンが押されました")
-       if ( switch_Y5 ) {
-         print("false")
-         switch_Y5 = false
-           Y5Button.backgroundColor = UIColor(red:1,green: 193/255,blue: 1,alpha: 1)
-       }
-       else {
-         print("true")
-         switch_Y5 = true
-           Y5Button.backgroundColor = UIColor.darkText
-       }
-     }
+//    @IBAction func touchButton_man(_ sender: Any) {
+//       print("ボタンが押されました")
+//       if ( switch_man ) {
+//         print("false")
+//         switch_man = false
+//           ManButton.backgroundColor = UIColor(red:1,green: 193/255,blue: 1,alpha: 1)
+//       }
+//       else {
+//         print("true")
+//         switch_man = true
+//           ManButton.backgroundColor = UIColor.darkText
+//       }
+//     }
+//
+//    @IBAction func touchButton_woman(_ sender: Any) {
+//       print("ボタンが押されました")
+//       if ( switch_woman ) {
+//         print("false")
+//         switch_woman = false
+//           womanButton.backgroundColor = UIColor(red:1,green: 193/255,blue: 1,alpha: 1)
+//       }
+//       else {
+//         print("true")
+//         switch_woman = true
+//           womanButton.backgroundColor = UIColor.darkText
+//       }
+//     }
+//
+//    @IBAction func touchButton_Y1(_ sender: Any) {
+//       print("ボタンが押されました")
+//       if ( switch_Y1 ) {
+//         print("false")
+//         switch_Y1 = false
+//           Y1Button.backgroundColor = UIColor(red:1,green: 193/255,blue: 1,alpha: 1)
+//       }
+//       else {
+//         print("true")
+//         switch_Y1 = true
+//           Y1Button.backgroundColor = UIColor.darkText
+//       }
+//     }
+//    @IBAction func touchButton_Y2(_ sender: Any) {
+//       print("ボタンが押されました")
+//       if ( switch_Y2 ) {
+//         print("false")
+//         switch_Y2 = false
+//           Y2Button.backgroundColor = UIColor(red:1,green: 193/255,blue: 1,alpha: 1)
+//       }
+//       else {
+//         print("true")
+//         switch_Y2 = true
+//           Y2Button.backgroundColor = UIColor.darkText
+//       }
+//     }
+//    @IBAction func touchButton_Y3(_ sender: Any) {
+//       print("ボタンが押されました")
+//       if ( switch_Y3 ) {
+//         print("false")
+//         switch_Y3 = false
+//           Y3Button.backgroundColor = UIColor(red:1,green: 193/255,blue: 1,alpha: 1)
+//       }
+//       else {
+//         print("true")
+//         switch_Y3 = true
+//           Y3Button.backgroundColor = UIColor.darkText
+//       }
+//     }
+//    @IBAction func touchButton_Y4(_ sender: Any) {
+//       print("ボタンが押されました")
+//       if ( switch_Y4 ) {
+//         print("false")
+//         switch_Y4 = false
+//           Y4Button.backgroundColor = UIColor(red:1,green: 193/255,blue: 1,alpha: 1)
+//       }
+//       else {
+//         print("true")
+//         switch_Y4 = true
+//           Y4Button.backgroundColor = UIColor.darkText
+//       }
+//     }
+//    @IBAction func touchButton_Y5(_ sender: Any) {
+//       print("ボタンが押されました")
+//       if ( switch_Y5 ) {
+//         print("false")
+//         switch_Y5 = false
+//           Y5Button.backgroundColor = UIColor(red:1,green: 193/255,blue: 1,alpha: 1)
+//       }
+//       else {
+//         print("true")
+//         switch_Y5 = true
+//           Y5Button.backgroundColor = UIColor.darkText
+//       }
+//     }
     
     
     }
